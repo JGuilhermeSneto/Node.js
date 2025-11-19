371cd174cdd3924beb67c1fa043131486e038936
José Guilherme da Silva Neto <20241198060033@ifrn.local>
2025-11-19 10:07:48 -0300

    Aula dia 19/11

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 // Para receber JSON do Postman ou fetch()
 app.use(express.json())
 
-// Para receber dados de formulário HTML (application/x-www-form-urlencoded)
+
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/prj01', (req, res) => {
@@ -13,10 +13,42 @@ app.get('/prj01', (req, res) => {
 })
 
 app.post('/prj02', (req, res) => {
-    // Agora funciona tanto para JSON quanto formulário
+    
     const nome = req.body.nome
     res.send(`Servidor web de ${nome} usando POST.`)
 })
+
+app.post('/prj03', (req, res) =>{
+    const id = req.body.id
+    const nome = req.body.nome
+    const dep = req.body.dependentes
+    let strdep = ''
+
+
+    for(i=0; i<dep.length; i++){
+        strdep += '['+ dep[i].nome + " | " +dep[i].par   + ']'
+    }
+
+
+    res.send(`<h1>o id enviado é ${id} e o nome enviado é ${nome}.
+                    Dependentes: ${strdep}</h1>`)
+})
+
+
+app.post('/prj04', (req, res) => {
+    const v1 = req.body.v1
+    const v2 = req.body.v2
+    const op = req.body.op
+    let r = 0
+
+    if (op == "+"){
+        r = parseInt(v1) + parseInt(v2)
+    }
+
+
+    res.json({"res" : r}) 
+})
+
 
 app.listen(3000, () => {
     console.log('Servidor executando na porta 3000...')
